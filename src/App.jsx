006084915d7c2c96b8c2707dd13098892dc9cd43
MutMartin <martinmut12@gmail.com>
2025-10-21 20:20:04 -0300

    primera entrega

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {navbar} from './components/NavBar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <navbar />
       <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+function navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Inicio</a></li>
+        <li><a href="#services">Servicios</a></li>
+        <li><a href="#contact">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
+export default navbar;
