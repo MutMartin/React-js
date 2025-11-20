c68184b2604d02c0b77247a899d571c978b27821
MutMartin <martinmut12@gmail.com>
2025-11-20 01:12:40 -0300

    Correcciones varias

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Container from './components/Container/Container.jsx';
-import ItemDetail from './components/ItemDetail/ItemDetail.jsx';
+
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Container />} />
-        <Route path="/item/:id" element={<ItemDetail />} />
+
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
